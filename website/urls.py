092e6d1bfec3ebc7f61f84d55b5ca2bb673b5d41
Govardhan <govardhangiri.m@gmail.com>
2025-02-10 14:03:51 +0530

    Initial commit - CRM Project

---
+++ website/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),  # ✅ Add this line
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('record/<int:pk>', views.customer_record, name='record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
+    path('add_record/', views.add_record, name='add_record'),
+    path('update_record/<int:pk>', views.update_record, name='update_record'),
+]
